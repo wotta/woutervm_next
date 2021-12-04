cbbb37d618552574ee60561d88609d71211670f7
Wouter <wouter.van.marrum@protonmail.com>
2021-12-04 22:56:16 +0100

    Initial base commit

--- frontend/components/nav.js
+++ frontend/components/nav.js
@@ -1,34 +1,58 @@
 import React from "react"
 import Link from "next/link"
+import {isCurrent} from "../lib/isCurrent";
 
 const Nav = ({ categories }) => {
   return (
-    <div>
-      <nav className="uk-navbar-container" data-uk-navbar>
-        <div className="uk-navbar-left">
-          <ul className="uk-navbar-nav">
-            <li>
-              <Link href="/">
-                <a>Strapi Blog</a>
-              </Link>
-            </li>
-          </ul>
+      <>
+        <nav className="flex px-11 items-center justify-between flex-wrap py-4 fixed w-full z-10 top-0 text-lg"
+            //x-data="{ isOpen: false }"
+            //@keydown.escape="isOpen = false"
+            //:class="{ 'shadow-lg bg-gray-200 dark:bg-grey-200' : isOpen , 'bg-gray-100 dark:bg-grey-100' : !isOpen}"
+        >
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <Link href="/">
+            <a class="text-white no-underline hover:text-white hover:no-underline">
+              <span class="bg-clip-text text-transparent bg-gradient-to-b from-pink-600 to-purple-600 text-5xl font-black motion-safe:animate-pulse"><i class="em em-grinning"></i> Wotta</span>
+            </a>
+          </Link>
         </div>
-        <div className="uk-navbar-right">
-          <ul className="uk-navbar-nav">
-            {categories.map((category) => {
-              return (
-                <li key={category.id}>
-                  <Link href={`/category/${category.attributes.slug}`}>
-                    <a className="uk-link-reset">{category.attributes.name}</a>
-                  </Link>
-                </li>
-              )
-            })}
+
+        <button type="button" class="block lg:hidden px-2 text-pink-600 hover:text-gray-600 focus:outline-none focus:text-gray-600"
+          //@click="isOpen = !isOpen"
+          //:class="{ 'transform-180': isOpen }"
+        >
+          <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path
+                // x-show="isOpen"
+                fillRule="evenodd" clipRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
+            <path
+                // x-show="!isOpen"
+                fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+          </svg>
+        </button>
+
+          <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto"
+            //:class="{ 'block shadow-3xl': isOpen, 'hidden': !isOpen }"
+            //@click.away="isOpen = false"
+            //x-show.transition="true"
+          >
+            {/*  //href="{{ url }}" */}
+            <ul class="pt-6 lg:pt-0 list-reset lg:flex justify-end flex-1 items-center">
+                {categories.map((category) => {
+                    let current = isCurrent(`/category/${category.attributes.slug}`);
+                  return (
+                    <li className="mr-3" key={category.id}>
+                      <Link href={`/category/${category.attributes.slug}`}>
+                        <a className={`inline-block py-2 px-4 no-underline text-xl ${current ? 'text-pink-600 font-medium' :  'text-gray-600 hover:text-pink-600 hover:text-underline'}`}>{category.attributes.name}</a>
+                      </Link>
+                    </li>
+                  )
+                })}
           </ul>
         </div>
       </nav>
-    </div>
+    </>
   )
 }
 
---
+++ frontend/lib/isCurrent.js
@@ -0,0 +1,10 @@
+import {useRouter} from "next/router";
+
+export function isCurrent(path) {
+    const { asPath, pathname } = useRouter();
+
+    console.log(asPath); // '/blog/xyz'
+    console.log(pathname); // '/blog/[slug]'
+
+    return asPath === path;
+}
