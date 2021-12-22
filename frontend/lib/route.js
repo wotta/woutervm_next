3c7dff4652d38d64a82488472d7411fc87204f86
Wouter <wouter.van.marrum@protonmail.com>
2021-12-22 22:11:40 +0100

    Removed the isCurrent helper and replaced it with the Route helper

---
+++ frontend/lib/route.js
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router"
+
+const Route = () => {
+  const { basePath, asPath, route } = useRouter()
+
+  return {
+    debug: () => {
+      console.log(`basePath: ${basePath}`)
+      console.log(`asPath: ${asPath}`)
+      console.log(`route: ${route}`)
+    },
+
+    isCurrent: (path) => {
+      return asPath === path
+    },
+  }
+}
+
+export default Route
