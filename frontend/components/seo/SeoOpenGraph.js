d5034c35001b286550f9758e46b6af7c115d2441
Wouter <wouter.van.marrum@protonmail.com>
2021-12-23 22:29:16 +0100

    Created the SeoOpenGraph component that can be used on content types.

---
+++ frontend/components/seo/SeoOpenGraph.js
@@ -0,0 +1,27 @@
+import Head from "next/head"
+import { getStrapiMedia } from "../../lib/media"
+import TwitterOpenGraph from "./TwitterOpenGraph"
+
+const SeoOpenGraph = ({ seo }) => {
+  return (
+    <>
+      <Head>
+        {seo.social_title && (
+          <meta property="og:title" content={seo.social_title} />
+        )}
+        {seo.social_description && (
+          <meta property="og:description" content={seo.social_description} />
+        )}
+        {seo.social_image && (
+          <meta
+            property="og:image"
+            content={getStrapiMedia(seo.social_image)}
+          />
+        )}
+      </Head>
+      <TwitterOpenGraph seo={seo} />
+    </>
+  )
+}
+
+export default SeoOpenGraph
