7fbe7a38d0133ee097ca67c39ea0b77a63b5412a
Wouter <wouter.van.marrum@protonmail.com>
2021-12-23 22:28:43 +0100

    Created a custom component for the Twitter open graph information.

---
+++ frontend/components/seo/TwitterOpenGraph.js
@@ -0,0 +1,51 @@
+import Head from "next/head"
+import Str from "../../lib/str"
+import { useContext } from "react"
+import { GlobalContext } from "../../pages/_app"
+import { getStrapiMedia } from "../../lib/media"
+
+const TwitterOpenGraph = ({ seo }) => {
+  const { twitterUsername } = useContext(GlobalContext)
+
+  let twitterHandle = Str(twitterUsername)
+    .when(twitterUsername.charAt(0) === "@", () => twitterUsername.substr(1))
+    .value()
+
+  return (
+    <Head>
+      {seo.social_title && (
+        <meta property="twitter:title" content={seo.social_title} />
+      )}
+
+      {seo.social_description && (
+        <meta property="twitter:description" content={seo.social_description} />
+      )}
+
+      <meta name="twitter:card" content="summary_large_image" />
+
+      {twitterHandle && (
+        <>
+          <meta name="twitter:site" content={`@${twitterHandle}`} />
+          <meta name="twitter:creator" content={`@${twitterHandle}`} />
+        </>
+      )}
+
+      {seo.twitter_image && (
+        <meta
+          property="twitter:image"
+          content={getStrapiMedia(seo.twitter_image)}
+        />
+      )}
+
+      {seo.twitter_image &&
+        seo.twitter_image.data.attributes.alternativeText && (
+          <meta
+            property="twitter:image:alt"
+            content={seo.twitter_image.data.attributes.alternativeText}
+          />
+        )}
+    </Head>
+  )
+}
+
+export default TwitterOpenGraph
