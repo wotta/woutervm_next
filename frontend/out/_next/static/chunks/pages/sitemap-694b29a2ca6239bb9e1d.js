68fafa54ab6af106e7d15939e71907593f75d063
Wouter <wouter.van.marrum@protonmail.com>
2021-12-27 23:45:52 +0100

    Ran next build and deploy

---
+++ frontend/out/_next/static/chunks/pages/sitemap-694b29a2ca6239bb9e1d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[115],{2681:function(n,u,t){"use strict";t.r(u);var r=t(5893);u.default=function(){return(0,r.jsx)(r.Fragment,{})}},9393:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/sitemap",function(){return t(2681)}])}},function(n){n.O(0,[774,888,179],(function(){return u=9393,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
