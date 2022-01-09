1d9204cda6e17b2093d6571a7c98e373c3e19b35
Wouter <wouter.van.marrum@protonmail.com>
2022-01-09 22:58:11 +0100

    Created basic blog page

---
+++ frontend/pages/blog.js
@@ -0,0 +1,55 @@
+import { fetchAPI } from "../lib/api"
+import { GlobalContext } from "./_app"
+import Layout from "../components/layout"
+import React, { useContext } from "react"
+import BlogMapper from "../mappers/BlogMapper"
+
+const BlogIndex = ({ blogs }) => {
+  const { navigation } = useContext(GlobalContext)
+
+  console.log(blogs)
+
+  return (
+    <Layout navigation={navigation}>
+      <div className="container mt-24 md:mt-18 p-8 rounded prose prose-pink prose-sm sm:prose-lg lg:prose-lg xl:prose-2xl mx-auto">
+        <main>
+          <h1>Blogs</h1>
+          <p>
+            You can find posts about things that I find interesting or wanted to
+            share my experience about.
+          </p>
+
+          <hr />
+
+          {blogs.map((_blog) => {
+            const blog = new BlogMapper(_blog)
+
+            return (
+              <div key={blog.getId()}>
+                <h2>{blog.getTitle()}</h2>
+                <p>
+                  <a href={blog.getSlug()}>Read more</a>
+                </p>
+              </div>
+            )
+          })}
+        </main>
+      </div>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const blogsRes = await fetchAPI("/blogs", {
+    fields: ["title", "slug"],
+  })
+
+  return {
+    props: {
+      blogs: blogsRes.data,
+    },
+    revalidate: 1,
+  }
+}
+
+export default BlogIndex
