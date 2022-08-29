47c29cbc343d34988deafdc48b11572562f0efa5
Wouter <wouter.van.marrum@protonmail.com>
2022-08-29 21:55:37 +0200

    Updated the blog post template

--- frontend/pages/blog/[slug]/index.js
+++ frontend/pages/blog/[slug]/index.js
@@ -1,11 +1,10 @@
 import Seo from "@/components/seo"
 import { fetchAPI } from "@/lib/api"
-import React, { useContext } from "react"
 import Layout from "@/components/layout"
+import React, { useContext } from "react"
 import ReactMarkdown from "react-markdown"
 import { GlobalContext } from "@/pages/_app"
 import BlogMapper from "@/mappers/BlogMapper"
-import remarkGfm from "remark-gfm"
 
 const BlogPage = ({ blog }) => {
   const { navigation } = useContext(GlobalContext)
@@ -17,15 +16,18 @@ const BlogPage = ({ blog }) => {
       <Seo seo={blog.getSeo()} />
       <div className="container mt-24 md:mt-18 p-8 rounded prose prose-pink prose-sm sm:prose-lg lg:prose-lg xl:prose-2xl mx-auto">
         <main>
+          <time
+            className="text-base muted font-normal"
+            dateTime={blog.getDateTime()}
+          >
+            {blog.getFormattedDate()}
+          </time>
           <h1>{blog.getTitle()}</h1>
           <article>
             {blog.getContent().map((_content) => {
               return (
                 <div key={_content.id}>
-                  <ReactMarkdown
-                    children={_content.content}
-                    // remarkPlugins={[remarkGfm]}
-                  />
+                  <ReactMarkdown>{_content.content}</ReactMarkdown>
                 </div>
               )
             })}
