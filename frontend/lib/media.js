178caa0700ca1571e4813376d0e148056c2bc6d8
Wouter van Marrum <wouter.van.marrum@datatrics.com>
2022-10-05 16:48:37 +0200

    chore(debug): Removed console log from media.js

--- frontend/lib/media.js
+++ frontend/lib/media.js
@@ -1,10 +1,6 @@
 import { getStrapiURL } from "./api"
 
 export function getStrapiMedia(media, debugInfo) {
-  if (process.env.DEBUG_MODE) {
-    console.log("getStrapiMedia", media, debugInfo)
-  }
-
   const { url } = media.data.attributes
   const imageUrl = url.startsWith("/") ? getStrapiURL(url) : url
   return imageUrl
