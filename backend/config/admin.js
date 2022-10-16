5163e2375e1a4556b2f194e6439468a455492e60
Wouter <wouter.van.marrum@protonmail.com>
2022-10-16 22:07:15 +0200

    feature(admin): Adding missing configuration

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,4 +1,9 @@
+const crypto = require("crypto");
+
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', crypto.randomBytes(16).toString('base64'))
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '147437602ea52ea22e20b5630a591079'),
   },
