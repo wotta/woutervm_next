f38f61dde55efba77f35739407ee8a3180fa3ab8
Wouter <wouter.van.marrum@protonmail.com>
2023-01-04 20:41:42 +0000

    feat: Run prettier

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    plugins: [
-        "postcss-import",
-        "tailwindcss",
-        "postcss-flexbugs-fixes",
-        "postcss-nesting",
-        "postcss-custom-properties",
-        "autoprefixer",
-    ],
+  plugins: [
+    "postcss-import",
+    "tailwindcss",
+    "postcss-flexbugs-fixes",
+    "postcss-nesting",
+    "postcss-custom-properties",
+    "autoprefixer",
+  ],
 }
